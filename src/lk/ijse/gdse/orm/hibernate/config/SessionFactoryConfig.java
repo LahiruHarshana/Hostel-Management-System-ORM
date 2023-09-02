b57d1109c4d381b5f70b8a8b04546f6d0c0b6f99
LahiruHarshana <lharshana2002@gmail.com>
2023-09-02 23:07:45 +0530

    0.3.5

--- src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
+++ src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.gdse.orm.hibernate.config;
 
+import lk.ijse.gdse.orm.hibernate.entity.Student;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
--- src/lk/ijse/gdse/orm/hibernate/entity/Student.java
+++ src/lk/ijse/gdse/orm/hibernate/entity/Student.java
@@ -1,3 +1,9 @@
 package lk.ijse.gdse.orm.hibernate.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
 public class Student {
 }
