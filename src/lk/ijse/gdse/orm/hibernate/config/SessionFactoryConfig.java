e65b78bbedcdec6182de6fc75bfb5b6c6c0da3d7
LahiruHarshana <lharshana2002@gmail.com>
2023-09-02 23:06:59 +0530

    0.3.4

--- src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
+++ src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
@@ -1,12 +1,13 @@
 package lk.ijse.gdse.orm.hibernate.config;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class SessionFactoryConfig {
     private static SessionFactoryConfig FactoryConfig;
     private final SessionFactory sessionFactory;
 
     private SessionFactoryConfig(){
-
+        sessionFactory = new Configuration().configure().addAnnotatedClass(Student.class).buildSessionFactory();
     }
 }
