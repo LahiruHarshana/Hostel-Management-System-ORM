9202f72cd2d998927fb82e1ae62a25cd6985ff34
LahiruHarshana <lharshana2002@gmail.com>
2023-09-04 12:35:40 +0530

    0.8.8

--- src/lk/ijse/gdse/orm/hibernate/entity/Room.java
+++ src/lk/ijse/gdse/orm/hibernate/entity/Room.java
@@ -2,11 +2,15 @@ package lk.ijse.gdse.orm.hibernate.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "room")
 public class Room {
-@Column(name = "room_type_id")
+    @Id
+    @Column(name = "room_type_id")
     private String id;
+
+@Column
 }
