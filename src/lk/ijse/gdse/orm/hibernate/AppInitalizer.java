befc5f5655d5e26c0f678e7476c1a56395d4413a
LahiruHarshana <lharshana2002@gmail.com>
2023-09-04 15:45:28 +0530

    1.3.2

--- src/lk/ijse/gdse/orm/hibernate/AppInitalizer.java
+++ src/lk/ijse/gdse/orm/hibernate/AppInitalizer.java
@@ -1,10 +1,28 @@
 package lk.ijse.gdse.orm.hibernate;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import lk.ijse.gdse.orm.hibernate.config.SessionFactoryConfig;
 
-public class AppInitalizer {
-    public static void main(String[] args) {
-        SessionFactoryConfig.getInstance().getSession();
-    }
+public class AppInitalizer extends Application {
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        SessionFactoryConfig.getInstance().getSession();
+
+        Image image=new Image("/img/train.png");
+        Parent parent =  FXMLLoader.load(getClass().getResource("/view/loginPage_form.fxml"));
+        stage.getIcons().add(image);
+        stage.setScene(new Scene(parent));
+        stage.setTitle("Railway Station");
+        stage.centerOnScreen();
+        stage.setResizable(false);
+
+        stage.show();
+    }
 }
