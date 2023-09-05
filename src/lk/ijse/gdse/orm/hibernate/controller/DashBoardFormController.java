f4fd38be4f074df2905ae18321be0b6d5112fa0d
LahiruHarshana <lharshana2002@gmail.com>
2023-09-05 23:38:12 +0530

    2.6.8

--- src/lk/ijse/gdse/orm/hibernate/controller/DashBoardFormController.java
+++ src/lk/ijse/gdse/orm/hibernate/controller/DashBoardFormController.java
@@ -1,5 +1,11 @@
 package lk.ijse.gdse.orm.hibernate.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class DashBoardFormController {
+    @FXML
+    private Button hmBtn;
+
 
 }
