6246820809d95be8570069f1b21fa481d01e5375
LahiruHarshana <lharshana2002@gmail.com>
2023-09-05 21:16:02 +0530

    2.3.7

--- src/lk/ijse/gdse/orm/hibernate/controller/SignUpFormController.java
+++ src/lk/ijse/gdse/orm/hibernate/controller/SignUpFormController.java
@@ -1,5 +1,11 @@
 package lk.ijse.gdse.orm.hibernate.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
 public class SignUpFormController {
 
+    @FXML
+    private TextField txtName;
 }
