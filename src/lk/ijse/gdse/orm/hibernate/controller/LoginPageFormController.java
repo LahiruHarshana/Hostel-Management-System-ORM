726ce17d505ec5a5f1d7de7dcaf8599939824b1c
LahiruHarshana <lharshana2002@gmail.com>
2023-09-05 13:45:28 +0530

    1.9.0

--- src/lk/ijse/gdse/orm/hibernate/controller/LoginPageFormController.java
+++ src/lk/ijse/gdse/orm/hibernate/controller/LoginPageFormController.java
@@ -1,4 +1,23 @@
 package lk.ijse.gdse.orm.hibernate.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginPageFormController {
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private PasswordField txtPswd;
+
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
 }
