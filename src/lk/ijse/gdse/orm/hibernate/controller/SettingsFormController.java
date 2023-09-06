ea0dbc1755fe4d953d0439408944c03ac22d6e03
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 21:41:53 +0530

    3.4.7

--- src/lk/ijse/gdse/orm/hibernate/controller/SettingsFormController.java
+++ src/lk/ijse/gdse/orm/hibernate/controller/SettingsFormController.java
@@ -1,4 +1,63 @@
 package lk.ijse.gdse.orm.hibernate.controller;
 
 public class SettingsFormController {
+    @FXML
+    private Line newPwLine;
+
+    @FXML
+    private PasswordField newPwTxt;
+
+    @FXML
+    private Button newPwViewBtn;
+
+    @FXML
+    private Line oldPwLine;
+
+    @FXML
+    private TextField oldPwTxt;
+
+    @FXML
+    private Line pwLine;
+
+    @FXML
+    private PasswordField pwTxt;
+
+    @FXML
+    private Button pwViewBtn;
+
+    @FXML
+    private ImageView viewImg;
+
+    @FXML
+    private ImageView viewImg1;
+
+    @FXML
+    void newPwTxtOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newPwViewBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void oldPwTxtOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pwTxtOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pwViewBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void svBtnOAction(ActionEvent event) {
+
+    }
 }
