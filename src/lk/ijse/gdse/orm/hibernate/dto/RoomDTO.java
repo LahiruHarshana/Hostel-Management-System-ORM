2cf958d50d1b596140d46930ea8fb2b4af32e65e
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 22:13:58 +0530

    3.7.9

--- src/lk/ijse/gdse/orm/hibernate/dto/RoomDTO.java
+++ src/lk/ijse/gdse/orm/hibernate/dto/RoomDTO.java
@@ -15,4 +15,36 @@ public class RoomDTO {
 
     public RoomDTO() {
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public String getKeyMoney() {
+        return keyMoney;
+    }
+
+    public void setKeyMoney(String keyMoney) {
+        this.keyMoney = keyMoney;
+    }
 }
