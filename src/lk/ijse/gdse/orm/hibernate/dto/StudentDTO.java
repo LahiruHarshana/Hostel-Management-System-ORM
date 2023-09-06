16609b5f46d0d8c4cbca050f7dcb4b60f2dc14ee
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 22:31:21 +0530

    4.0.4

--- src/lk/ijse/gdse/orm/hibernate/dto/StudentDTO.java
+++ src/lk/ijse/gdse/orm/hibernate/dto/StudentDTO.java
@@ -22,4 +22,52 @@ public class StudentDTO {
 
     public StudentDTO() {
     }
+
+    public String getsId() {
+        return sId;
+    }
+
+    public void setsId(String sId) {
+        this.sId = sId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getGen() {
+        return gen;
+    }
+
+    public void setGen(String gen) {
+        this.gen = gen;
+    }
 }
