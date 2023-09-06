d016035450a7899eedeebe55bd99ede054b57063
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 22:38:02 +0530

    4.1.8

---
+++ src/lk/ijse/gdse/orm/hibernate/bo/custom/RoomBO.java
@@ -0,0 +1,18 @@
+package lk.ijse.gdse.orm.hibernate.bo.custom;
+
+
+import lk.ijse.gdse.orm.hibernate.bo.SuperBo;
+
+import java.util.List;
+
+public interface RoomBO extends SuperBo {
+    List<RoomDTO> getAll();
+
+    boolean saveRoom(RoomDTO roomDTO);
+
+    boolean updateRoom(RoomDTO roomDTO);
+
+    RoomDTO getRoom(String rid);
+
+    boolean deleteRoom(String text);
+}
