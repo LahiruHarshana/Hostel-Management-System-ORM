77da48105ffc634bd335c49721298328308a3b7b
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 23:04:11 +0530

    4.4.2

--- src/lk/ijse/gdse/orm/hibernate/controller/DashBoardFormController.java
+++ src/lk/ijse/gdse/orm/hibernate/controller/DashBoardFormController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.gdse.orm.hibernate.controller.util.Navigation;
 import lk.ijse.gdse.orm.hibernate.controller.util.Rout;
 
 import java.io.IOException;
