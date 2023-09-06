7711e54ff7c0037813b59e7c52399304e177da0e
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 22:12:10 +0530

    3.7.1

--- src/lk/ijse/gdse/orm/hibernate/dto/tm/StudentTM.java
+++ src/lk/ijse/gdse/orm/hibernate/dto/tm/StudentTM.java
@@ -21,4 +21,52 @@ public class StudentTM {
 
     public StudentTM() {
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
