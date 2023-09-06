121524c10716796f26d867e14d793e0d5d86185e
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 21:45:42 +0530

    3.5.6

--- src/lk/ijse/gdse/orm/hibernate/controller/StdFormController.java
+++ src/lk/ijse/gdse/orm/hibernate/controller/StdFormController.java
@@ -3,6 +3,7 @@ package lk.ijse.gdse.orm.hibernate.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 public class StdFormController {
     @FXML
