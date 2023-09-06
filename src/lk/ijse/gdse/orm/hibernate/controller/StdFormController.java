a56f3fcfc154ba61961fd5991cbe44594ba830ad
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 21:45:05 +0530

    3.5.2

--- src/lk/ijse/gdse/orm/hibernate/controller/StdFormController.java
+++ src/lk/ijse/gdse/orm/hibernate/controller/StdFormController.java
@@ -1,4 +1,74 @@
 package lk.ijse.gdse.orm.hibernate.controller;
 
 public class StdFormController {
+    @FXML
+    private Button addBtn1;
+
+    @FXML
+    private TableColumn<?, ?> addressColm;
+
+    @FXML
+    private Line addressLine;
+
+    @FXML
+    private TextField addressTxt;
+
+    @FXML
+    private TableColumn<?, ?> contactColm;
+
+    @FXML
+    private Line contactLine;
+
+    @FXML
+    private TextField contactTxt;
+
+    @FXML
+    private Button deleteBtn;
+
+    @FXML
+    private TableColumn<?, ?> dobColm;
+
+    @FXML
+    private DatePicker dobPicker;
+
+    @FXML
+    private TableColumn<?, ?> genColm;
+
+    @FXML
+    private Line genLine;
+
+    @FXML
+    private TextField genTxt;
+
+    @FXML
+    private TableColumn<?, ?> idColm;
+
+    @FXML
+    private Line idLine;
+
+    @FXML
+    private TextField idTxt;
+
+    @FXML
+    private TableColumn<?, ?> nameColm;
+
+    @FXML
+    private Line nameLine;
+
+    @FXML
+    private TextField nameTxt;
+
+    @FXML
+    private TextField searchTxt;
+    @FXML
+    private Button searchBtn;
+
+    @FXML
+    private TableView<StudentTM> stdTbl;
+
+    @FXML
+    private Button svBtn;
+
+    @FXML
+    private Button upBtn;
 }
