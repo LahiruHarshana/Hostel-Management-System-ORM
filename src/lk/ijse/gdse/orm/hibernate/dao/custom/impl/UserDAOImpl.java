b4f50988e382dd5dbff021820703101077151198
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 22:26:59 +0530

    3.9.8

---
+++ src/lk/ijse/gdse/orm/hibernate/dao/custom/impl/UserDAOImpl.java
@@ -0,0 +1,46 @@
+package lk.ijse.gdse.orm.hibernate.dao.custom.impl;
+
+import lk.ijse.gdse.orm.hibernate.dao.custom.UserDAO;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class UserDAOImpl implements UserDAO {
+    @Override
+    public boolean save(User dto) {
+        Session session = SessionFactoryConfig.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        Serializable save = session.save(dto);
+        transaction.commit();
+        session.close();
+        return save != null;
+    }
+
+    @Override
+    public boolean update(User dto) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return false;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Override
+    public User getItem(String id) {
+        try (Session session = SessionFactoryConfig.getInstance().getSession()) {
+            Transaction transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            transaction.commit();
+            return user;
+        }
+
+    }
+}
