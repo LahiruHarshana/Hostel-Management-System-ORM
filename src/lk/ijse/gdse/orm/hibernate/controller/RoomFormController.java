9a14b8c7c890d85f26f508b80f319e03e8a43801
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 20:47:29 +0530

    3.1.3

--- src/lk/ijse/gdse/orm/hibernate/controller/RoomFormController.java
+++ src/lk/ijse/gdse/orm/hibernate/controller/RoomFormController.java
@@ -2,12 +2,17 @@ package lk.ijse.gdse.orm.hibernate.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 
 public class RoomFormController {
     @FXML
     private Button addBtn1;
     @FXML
     private Button deleteBtn;
+
+    @FXML
+    private TableColumn<?, ?> idColm;
+
 
 
 
