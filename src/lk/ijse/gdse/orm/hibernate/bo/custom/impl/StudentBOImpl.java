e3e6dc4d15f1d81ba33080c7efd75d7aa1a78b86
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 22:39:48 +0530

    4.2.5

---
+++ src/lk/ijse/gdse/orm/hibernate/bo/custom/impl/StudentBOImpl.java
@@ -0,0 +1,76 @@
+package lk.ijse.gdse.orm.hibernate.bo.custom.impl;
+
+
+import lk.ijse.gdse.orm.hibernate.bo.custom.StudentBO;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+public class StudentBOImpl implements StudentBO {
+    StudentDAO studentDAO = DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.STUDENT);
+
+    @Override
+    public List<StudentDTO> getAll() {
+        List<StudentDTO> studentDTOS = new ArrayList<>();
+        for (Student student : studentDAO.getAll()) {
+            studentDTOS.add(new StudentDTO(
+                    student.getId(),
+                    student.getName(),
+                    student.getAddress(),
+                    student.getContact(),
+                    student.getDob(),
+                    student.getGender())
+            );
+        }
+        return studentDTOS;
+    }
+
+    @Override
+    public boolean saveStd(StudentDTO studentDTO) {
+        return studentDAO.save(new Student(
+                studentDTO.getSId(),
+                studentDTO.getName(),
+                studentDTO.getAddress(),
+                studentDTO.getContact(),
+                studentDTO.getDob(),
+                studentDTO.getGen()
+        ));
+    }
+
+    @Override
+    public boolean updateStd(StudentDTO studentDTO) {
+        return studentDAO.update(new Student(
+                studentDTO.getSId(),
+                studentDTO.getName(),
+                studentDTO.getAddress(),
+                studentDTO.getContact(),
+                studentDTO.getDob(),
+                studentDTO.getGen()
+        ));
+    }
+
+    @Override
+    public StudentDTO getStudent(String sid) {
+        Student student = studentDAO.getItem(sid);
+        if (student!=null){
+            return new StudentDTO(
+                    student.getId(),
+                    student.getName(),
+                    student.getAddress(),
+                    student.getContact(),
+                    student.getDob(),
+                    student.getGender()
+            );
+        }
+        return null;
+    }
+
+    @Override
+    public boolean deleteStd(String text) {
+        return studentDAO.delete(text);
+    }
+}
