c996b9eb73317110e07cf60b5aef9fd672e5f883
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 22:50:02 +0530

    4.3.6

---
+++ src/lk/ijse/gdse/orm/hibernate/bo/BoFactory.java
@@ -0,0 +1,29 @@
+package lk.ijse.gdse.orm.hibernate.bo;
+
+import lk.ijse.gdse.orm.hibernate.bo.custom.impl.UserBoImpl;
+
+public class BoFactory {
+    private static BoFactory factory;
+
+    private BoFactory(){}
+
+    public static BoFactory getInstance(){
+        return factory == null ? new BoFactory() : factory;
+    }
+
+    public enum BOTypes{
+        USER, STUDENT, ROOM
+    }
+    public <T extends SuperBo>T getBo(BOTypes types){
+        switch (types){
+            case USER:
+                return (T) new UserBoImpl();
+            case STUDENT:
+                return (T) new StudentBOImpl();
+            case ROOM:
+                return (T) new RoomBOImpl();
+            default:
+                return null;
+        }
+    }
+}
