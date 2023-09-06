d988768a13c3c422f7d3a4186e0041ea13d8ec12
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 22:19:31 +0530

    3.8.6

---
+++ src/lk/ijse/gdse/orm/hibernate/dao/DAOFactory.java
@@ -0,0 +1,33 @@
+package lk.ijse.gdse.orm.hibernate.dao;
+
+import lk.ijse.bo.custom.impl.UserBoImpl;
+import lk.ijse.dao.custom.impl.RoomDAOImpl;
+import lk.ijse.dao.custom.impl.StudentDAOImpl;
+import lk.ijse.dao.custom.impl.UserDAOImpl;
+
+public class DAOFactory {
+    private static DAOFactory factory;
+
+    private DAOFactory(){}
+
+    public static DAOFactory getInstance(){
+        return factory == null ? new DAOFactory() : factory;
+    }
+
+    public enum DAOTypes{
+        USER, STUDENT, ROOM
+    }
+
+    public <T extends SuperDAO>T getDAO(DAOTypes types){
+        switch (types){
+            case USER:
+                return (T) new UserDAOImpl();
+            case STUDENT:
+                return (T) new StudentDAOImpl();
+            case ROOM:
+                return (T) new RoomDAOImpl();
+            default:
+                return null;
+        }
+    }
+}
