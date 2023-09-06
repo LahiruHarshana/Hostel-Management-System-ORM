5b9a001c9c488a435a180e1721306c28145ca5d4
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 22:33:21 +0530

    4.0.9

--- src/lk/ijse/gdse/orm/hibernate/dto/UserDTO.java
+++ src/lk/ijse/gdse/orm/hibernate/dto/UserDTO.java
@@ -11,4 +11,20 @@ public class UserDTO {
 
     public UserDTO() {
     }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
