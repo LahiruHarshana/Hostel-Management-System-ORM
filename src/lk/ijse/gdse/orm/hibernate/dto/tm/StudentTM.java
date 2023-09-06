220eee63877e393c71f15e8ac7688bf0f759ad9a
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 22:11:21 +0530

    3.6.8

--- src/lk/ijse/gdse/orm/hibernate/dto/tm/StudentTM.java
+++ src/lk/ijse/gdse/orm/hibernate/dto/tm/StudentTM.java
@@ -1,4 +1,12 @@
 package lk.ijse.gdse.orm.hibernate.dto.tm;
 
+import java.sql.Date;
+
 public class StudentTM {
+    private String id;
+    private String name;
+    private Date dob;
+    private String contact;
+    private String address;
+    private String gender;
 }
