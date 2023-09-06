9390d5baf4f291f5e2cc9d51621ee5171ed88d51
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 22:23:03 +0530

    3.9.1

--- src/lk/ijse/gdse/orm/hibernate/dao/custom/UserDAO.java
+++ src/lk/ijse/gdse/orm/hibernate/dao/custom/UserDAO.java
@@ -1,5 +1,6 @@
 package lk.ijse.gdse.orm.hibernate.dao.custom;
 
+import lk.ijse.gdse.orm.hibernate.dao.CrudDAO;
 import lk.ijse.gdse.orm.hibernate.entity.User;
 
 public interface UserDAO extends CrudDAO<User,String> {
