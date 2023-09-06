37f43d3965b2b172a6d12896f857270c8ee6dc39
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 22:30:58 +0530

    4.0.2

--- src/lk/ijse/gdse/orm/hibernate/dto/StudentDTO.java
+++ src/lk/ijse/gdse/orm/hibernate/dto/StudentDTO.java
@@ -1,12 +1,8 @@
 package lk.ijse.gdse.orm.hibernate.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
 import java.sql.Date;
 
-@Data
-@AllArgsConstructor
 public class StudentDTO {
     private String sId;
     private String name;
@@ -15,4 +11,12 @@ public class StudentDTO {
     private Date dob;
     private String gen;
 
+    public StudentDTO(String sId, String name, String address, String contact, Date dob, String gen) {
+        this.sId = sId;
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+        this.dob = dob;
+        this.gen = gen;
+    }
 }
