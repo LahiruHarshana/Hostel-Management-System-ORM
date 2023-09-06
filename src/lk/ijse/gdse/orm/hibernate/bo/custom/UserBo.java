29655676dad3ec4381cdd0fa5cbb3fd0592133e8
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 22:36:11 +0530

    4.1.3

--- src/lk/ijse/gdse/orm/hibernate/bo/custom/UserBo.java
+++ src/lk/ijse/gdse/orm/hibernate/bo/custom/UserBo.java
@@ -1,6 +1,7 @@
 package lk.ijse.gdse.orm.hibernate.bo.custom;
 
 import lk.ijse.gdse.orm.hibernate.bo.SuperBo;
+import lk.ijse.gdse.orm.hibernate.dto.UserDTO;
 
 public interface UserBo extends SuperBo {
 
