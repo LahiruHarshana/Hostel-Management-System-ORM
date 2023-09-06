fe0dd6e7beef3d1717a6895518aadb5b075109d6
LahiruHarshana <lharshana2002@gmail.com>
2023-09-06 22:20:48 +0530

    3.8.8

---
+++ src/lk/ijse/gdse/orm/hibernate/dao/custom/StudentDAO.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse.orm.hibernate.dao.custom;
+
+import lk.ijse.dao.CrudDAO;
+import lk.ijse.dao.SuperDAO;
+import lk.ijse.entity.Student;
+
+public interface StudentDAO extends CrudDAO<Student, String> {
+}
