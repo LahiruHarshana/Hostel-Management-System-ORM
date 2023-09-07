22cf145acd869856d7548c5713f54b8d9afd5318
LahiruHarshana <lharshana2002@gmail.com>
2023-09-07 20:31:33 +0530

    4.7.2

--- src/lk/ijse/gdse/orm/hibernate/controller/StdFormController.java
+++ src/lk/ijse/gdse/orm/hibernate/controller/StdFormController.java
@@ -1,9 +1,18 @@
 package lk.ijse.gdse.orm.hibernate.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import lk.ijse.gdse.orm.hibernate.bo.BoFactory;
+import lk.ijse.gdse.orm.hibernate.bo.custom.StudentBO;
+import lk.ijse.gdse.orm.hibernate.controller.util.CustomAlert;
+import lk.ijse.gdse.orm.hibernate.dto.StudentDTO;
 import lk.ijse.gdse.orm.hibernate.dto.tm.StudentTM;
+
+import java.sql.Date;
 
 public class StdFormController {
     @FXML
@@ -65,20 +74,123 @@ public class StdFormController {
     @FXML
     private Button upBtn;
 
+    private final StudentBO studentBO= BoFactory.getInstance().getBo(BoFactory.BOTypes.STUDENT);
+
+
+
+    private void fillTable() {
+        ObservableList<StudentTM> studentTMS = FXCollections.observableArrayList();
+        for (StudentDTO studentDTO : studentBO.getAll()) {
+            studentTMS.add(new StudentTM(
+                    studentDTO.getsId(),
+                    studentDTO.getName(),
+                    studentDTO.getDob(),
+                    studentDTO.getContact(),
+                    studentDTO.getAddress(),
+                    studentDTO.getGen())
+            );
+        }
+        stdTbl.setItems(studentTMS);
+    }
+
+    private void setCellValueFactory() {
+        idColm.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameColm.setCellValueFactory(new PropertyValueFactory<>("name"));
+        genColm.setCellValueFactory(new PropertyValueFactory<>("gender"));
+        dobColm.setCellValueFactory(new PropertyValueFactory<>("dob"));
+        contactColm.setCellValueFactory(new PropertyValueFactory<>("contact"));
+        addressColm.setCellValueFactory(new PropertyValueFactory<>("address"));
+    }
+
+    private void initUi(){
+        idTxt.clear();
+        nameTxt.clear();
+        genTxt.clear();
+        dobPicker.setValue(null);
+        contactTxt.clear();
+        addressTxt.clear();
+        idTxt.setDisable(true);
+        nameTxt.setDisable(true);
+        genTxt.setDisable(true);
+        dobPicker.setDisable(true);
+        contactTxt.setDisable(true);
+        addressTxt.setDisable(true);
+        deleteBtn.setDisable(true);
+        upBtn.setDisable(true);
+        svBtn.setDisable(true);
+    }
+
+    @FXML
+    void addNewBtnOnAction(ActionEvent event) {
+        idTxt.setDisable(false);
+        nameTxt.setDisable(false);
+        genTxt.setDisable(false);
+        dobPicker.setDisable(false);
+        contactTxt.setDisable(false);
+        addressTxt.setDisable(false);
+        idTxt.requestFocus();
+        svBtn.setDisable(false);
+    }
+
     @FXML
     void deleteBtnOnAction(ActionEvent event) {
+        if (studentBO.deleteStd(idTxt.getText())){
+            new CustomAlert(Alert.AlertType.CONFIRMATION,"Delete ","Deleted !","Student Deleted successful !").show();
+            fillTable();
+            initUi();
+        }else {
+            new CustomAlert(Alert.AlertType.ERROR,"Delete ","Not Deleted !","Delete not successful !").show();
+        }
     }
 
     @FXML
     void upBtnOnAction(ActionEvent event) {
+        if (studentBO.updateStd(new StudentDTO(idTxt.getText(),nameTxt.getText(),addressTxt.getText(),contactTxt.getText(), Date.valueOf(dobPicker.getValue()),genTxt.getText()))){
+            new CustomAlert(Alert.AlertType.CONFIRMATION,"Update ","Updated !","Student Update successful !").show();
+            fillTable();
+            initUi();
+        }else {
+            new CustomAlert(Alert.AlertType.ERROR,"Update ","Not Update !","Update not successful !").show();
+        }
     }
 
     @FXML
     void svBtnOnAction(ActionEvent event) {
+        if (studentBO.saveStd(new StudentDTO(idTxt.getText(),nameTxt.getText(),addressTxt.getText(),contactTxt.getText(),Date.valueOf(dobPicker.getValue()),genTxt.getText()))){
+            new CustomAlert(Alert.AlertType.CONFIRMATION,"Update ","Updated !","Student Update successful !").show();
+            fillTable();
+            initUi();
+        }else {
+            new CustomAlert(Alert.AlertType.ERROR,"Update ","Not Update !","Update not successful !").show();
+        }
     }
 
     @FXML
     void searchBtnOnCtion(ActionEvent event) {
+        String sid=idTxt.getText();
+        StudentDTO studentDTO=studentBO.getStudent(sid);
+        if (studentDTO!=null){
+            svBtn.setDisable(true);
+            upBtn.setDisable(false);
+            deleteBtn.setDisable(false);
+            idTxt.setDisable(true);
+            nameTxt.setDisable(false);
+            genTxt.setDisable(false);
+            dobPicker.setDisable(false);
+            contactTxt.setDisable(false);
+            addressTxt.setDisable(false);
+            idTxt.requestFocus();
 
+            idTxt.setText(studentDTO.getsId());
+            nameTxt.setText(studentDTO.getName());
+            genTxt.setText(studentDTO.getGen());
+            dobPicker.setValue(studentDTO.getDob().toLocalDate());
+            contactTxt.setText(studentDTO.getContact());
+            addressTxt.setText(studentDTO.getAddress());
+        }else {
+            new CustomAlert(Alert.AlertType.ERROR,"Error ","Invalid","Invalid student id !").show();
+        }
+        searchTxt.clear();
     }
+
 }
---
+++ src/lk/ijse/gdse/orm/hibernate/controller/util/CustomAlert.java
@@ -0,0 +1,34 @@
+package lk.ijse.gdse.orm.hibernate.controller.util;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class CustomAlert extends Alert {
+    public CustomAlert(AlertType alertType, String title, String header, String message, ButtonType... buttonTypes) {
+        super(alertType, message, buttonTypes);
+        setTitle(title);
+        setHeaderText(header);
+
+        String image = null;
+        switch (alertType) {
+            case ERROR:
+                image = "/assets/close.png";
+                break;
+            case INFORMATION:
+                image = "/assets/information-button.png";
+                break;
+            case WARNING:
+                image = "/assets/warning.png";
+                break;
+        }
+        if (image !=null){
+            ImageView imgView = new ImageView(new Image(image));
+            imgView.setFitWidth(40);
+            imgView.setFitHeight(40);
+            setGraphic(imgView);
+        }
+    }
+
+}
