019432259a7410651e4442162f654da7beee9338
LahiruHarshana <lharshana2002@gmail.com>
2023-09-07 22:22:39 +0530

    4.7.56

--- src/lk/ijse/gdse/orm/hibernate/entity/Reservation.java
+++ src/lk/ijse/gdse/orm/hibernate/entity/Reservation.java
@@ -28,4 +28,76 @@ public class Reservation implements SuperEntity{
 
    /* @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY,mappedBy = "reservation")
     List<StudentReservation> reservations = new ArrayList<>();*/
+
+    public Reservation() {
+    }
+
+    public Reservation(String reservationID, Date date, String status, Date expDate, Student student, Room room) {
+        this.reservationID = reservationID;
+        this.date = date;
+        this.status = status;
+        this.expDate = expDate;
+        this.student = student;
+        this.room = room;
+    }
+
+    public String getReservationID() {
+        return reservationID;
+    }
+
+    public void setReservationID(String reservationID) {
+        this.reservationID = reservationID;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(Date expDate) {
+        this.expDate = expDate;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "reservationID='" + reservationID + '\'' +
+                ", date=" + date +
+                ", status='" + status + '\'' +
+                ", expDate=" + expDate +
+                ", student=" + student +
+                ", room=" + room +
+                '}';
+    }
 }
