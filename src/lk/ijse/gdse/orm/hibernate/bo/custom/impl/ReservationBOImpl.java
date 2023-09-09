aa01f769456172f983fbcff4ffa066a5c9d45343
LahiruHarshana <lharshana2002@gmail.com>
2023-09-09 21:52:04 +0530

    fxml editing

--- src/lk/ijse/gdse/orm/hibernate/bo/custom/impl/ReservationBOImpl.java
+++ src/lk/ijse/gdse/orm/hibernate/bo/custom/impl/ReservationBOImpl.java
@@ -9,6 +9,7 @@ import lk.ijse.gdse.orm.hibernate.dao.custom.RoomDAO;
 import lk.ijse.gdse.orm.hibernate.dao.custom.StudentDAO;
 import lk.ijse.gdse.orm.hibernate.dto.ReservationDTO;
 import lk.ijse.gdse.orm.hibernate.dto.RoomDTO;
+import lk.ijse.gdse.orm.hibernate.dto.StudentDTO;
 import lk.ijse.gdse.orm.hibernate.entity.Room;
 import lk.ijse.gdse.orm.hibernate.entity.Student;
 
